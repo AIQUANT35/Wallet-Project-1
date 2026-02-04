b6e7620e2bf8437fbb81be11e8bb3cfb57d90628
Rupsha Ray <rupsha.ray@aiquantgroup.com>
2026-02-04 16:19:27 +0530

    Implemented lazy loading, added swagger API docs

--- node-backend/swagger.js
+++ node-backend/swagger.js
@@ -1,33 +1,3 @@
-// const swaggerJSDoc = require("swagger-jsdoc");
-
-// const options = {
-//   definition: {
-//     openapi: "3.0.0",
-
-//     info: {
-//       title: "Wallet API",
-//       version: "1.0.0",
-//       description: "Wallet + NFT + Transaction Backend APIs",
-//     },
-
-//     servers: [
-//       {
-//         url: "http://localhost:5000",
-//       },
-//     ],
-//   },
-
-//   apis: ["./server.js"], // tells swagger to scan server.js
-// };
-
-// const swaggerSpec = swaggerJSDoc(options);
-
-// module.exports = swaggerSpec;
-
-
-
-
-
 const swaggerJSDoc = require("swagger-jsdoc");
 
 const options = {
